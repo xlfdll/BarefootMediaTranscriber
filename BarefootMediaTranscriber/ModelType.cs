7af54b66eedad042f831c91fe6f704ffed01c920
xlfdll <xlfdll@gmail.com>
2023-04-11 08:41:12 +0900

    Added multilingual model support

--- BarefootMediaTranscriber/ModelType.cs
+++ BarefootMediaTranscriber/ModelType.cs
@@ -6,5 +6,12 @@
         public const String BaseEnglish = "base.en";
         public const String SmallEnglish = "small.en";
         public const String MediumEnglish = "medium.en";
+
+        public const String TinyMultilingual = "tiny";
+        public const String BaseMultilingual = "base";
+        public const String SmallMultilingual = "small";
+        public const String MediumMultilingual = "medium";
+
+        public const String Large = "large";
     }
 }
